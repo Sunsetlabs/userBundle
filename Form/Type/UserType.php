64b3afcb7adb1017bdbf5b590359b7927cb7ea42
Alejo Rodriguez <a.rodsott@gmail.com>
2015-05-29 13:17:10 -0300

    password ignore on edit

--- Form/Type/UserType.php
+++ Form/Type/UserType.php
@@ -5,12 +5,17 @@ namespace Sunsetlabs\UserBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class UserType extends AbstractType
 {
 	protected $user_class;
 	protected $form_fields;
 	protected $add_address;
+    protected $new = false;
 
 	public function __construct($user_class, $form_fields, $add_address)
 	{
@@ -21,9 +26,16 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
     	foreach ($this->form_fields as $field) {
-			$builder->add($field['property'], $field['type'], array(
-				'label' => $field['label']
-			));
+            if ($field['property'] == 'password') {
+                $builder->add('password', 'password', array(
+                    'label' => $field['label'],
+                    'required' => false
+                ));
+            }else{
+    			$builder->add($field['property'], $field['type'], array(
+    				'label' => $field['label']
+    			));
+            }
     	}
 
     	if ($this->add_address) {
@@ -34,6 +46,36 @@ class UserType extends AbstractType
     			'by_reference' => false
     		));
     	}
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'preSetData'));
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'preSubmit'));
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $user = $event->getData();
+
+        if (!$user->getId()) {
+            $this->new = true;
+            $form->add('password', 'password', array(
+                'required' => false,
+                'constraints' => array(
+                    new NotBlank()
+                )
+            ));
+        }
+    }
+    public function preSubmit(FormEvent $event)
+    {
+       $form = $event->getForm();
+       $user = $event->getData();
+       if (!$this->new and (!isset($user['password']) or empty($user['password']))) {
+            $form->add('password', 'password', array(
+                'required' => false,
+                'disabled' => true
+            ));
+       }   
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
